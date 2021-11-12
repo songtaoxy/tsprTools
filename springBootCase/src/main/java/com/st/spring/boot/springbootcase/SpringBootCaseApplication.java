3985e0521ac611c887604c3187136cda60b14989
songtao <songtaoxy@foxmail.com>
2021-11-12 12:50:17 +0800

    基础工具改造

---
+++ springBootCase/src/main/java/com/st/spring/boot/springbootcase/SpringBootCaseApplication.java
@@ -0,0 +1,12 @@
+package com.st.spring.boot.springbootcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCaseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootCaseApplication.class, args);
+  }
+}
---
+++ springBootCase/src/test/java/com/st/spring/boot/springbootcase/SpringBootCaseApplicationTests.java
@@ -0,0 +1,11 @@
+package com.st.spring.boot.springbootcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCaseApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
