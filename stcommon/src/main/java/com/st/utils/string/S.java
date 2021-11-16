69e9e1f613b0deee8a55f9bba875d4344be1e206
songtao <songtaoxy@foxmail.com>
2021-11-17 02:44:33 +0800

    工具类-符号类

---
+++ stcommon/src/main/java/com/st/utils/string/S.java
@@ -0,0 +1,26 @@
+package com.st.utils.string;
+
+/**
+ * S: symbol
+ *
+ * <p>单个字符, 在引用时, 占据的空间小.
+ *
+ * @author: st
+ * @date: 2021/11/17 02:31
+ * @version: 1.0
+ * @description:
+ */
+public class S {
+
+  /** l: Line */
+  public static final String l = "\n";
+
+  /** b: blank */
+  public static final String b = " ";
+
+  /** s: split line */
+  public static final String s = "\n==================================\n";
+
+  public static final String ss = "\n================= start =================\n";
+  public static final String se = "\n=================  end  =================\n";
+}
---
+++ stcommon/src/test/java/com/st/utils/string/STest.java
@@ -0,0 +1,17 @@
+package com.st.utils.string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class STest {
+
+  @Test
+  void bTest() {
+    System.out.println(S.l+"hi"+S.l);
+    System.out.println(S.b+"hi"+S.b);
+    System.out.println(S.s+"hi"+S.s);
+    System.out.println(S.ss+"hi"+S.se);
+  }
+
+}
