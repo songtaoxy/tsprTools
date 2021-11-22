87f8cec21066ddb79187e6706fc199e963b06c8a
songtao <songtaoxy@foxmail.com>
2021-11-22 14:51:42 +0800

    Logback 测试

---
+++ stcommon/src/main/java/com/st/practice/logback/LogbackDemo.java
@@ -0,0 +1,18 @@
+package com.st.practice.logback;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author: st
+ * @date: 2021/11/22 10:55
+ * @version: 1.0
+ * @description:
+ */
+@Slf4j
+public class LogbackDemo {
+
+  public static void main(String[] args) {
+    log.info("hi");
+    log.debug("hi");
+  }
+}
---
+++ stcommon/src/test/java/com/st/practice/logback/LogbackDemoTest.java
@@ -0,0 +1,11 @@
+package com.st.practice.logback;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogbackDemoTest {
+
+  @Test
+  void logbackLevel() {}
+}
