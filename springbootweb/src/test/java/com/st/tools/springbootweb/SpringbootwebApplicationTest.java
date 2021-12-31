c47a7d82ea52f40394d72a9ef6dfb3ade37524e5
songtao <songtaoxy@foxmail.com>
2022-01-01 06:56:43 +0800

    logback.xml: 避免logback本身日志输出

--- springbootweb/src/test/java/com/st/tools/springbootweb/SpringbootwebApplicationTest.java
+++ springbootweb/src/test/java/com/st/tools/springbootweb/SpringbootwebApplicationTest.java
@@ -1,26 +1,33 @@
 package com.st.tools.springbootweb;
 
+import com.oracle.tools.packager.Log;
 import com.st.tools.springbootweb.mapper.UsersMapper;
 import com.st.tools.springbootweb.vo.Users;
 import com.st.utils.log.LogUtils;
+import jdk.nashorn.api.scripting.ScriptUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SpringbootwebApplicationTest {
 
+  @Autowired DataSource dataSource;
   @Autowired UsersMapper usersMapper;
 
-	@Test
-	void testMytatisPlus(){
+  @Test
+  void testMytatisPlus() {
 
-		Users users = usersMapper.selectById(3L);
+    Users user = usersMapper.selectById(3L);
+    List<Users> users = usersMapper.selectList(null);
 
-		LogUtils.foal(users,"");
-		
-	}
-
+    LogUtils.foal(dataSource.getClass(), "datasource type");
+    LogUtils.foal(user, "");
+    LogUtils.foal(users, "");
+  }
 }
