ef242624602363c1fdfb2b9f0e892985ada32e40
songtao <songtaoxy@foxmail.com>
2022-03-13 03:25:09 +0800

    算法题: 山脉数组的峰顶索引

--- stcommon/src/main/java/com/st/dsa/MountainArray.java
+++ stcommon/src/main/java/com/st/dsa/MountainArray.java
@@ -1,6 +1,9 @@
 package com.st.dsa;
 
 import com.google.common.collect.PeekingIterator;
+import com.google.common.primitives.Ints;
+
+import java.util.Arrays;
 
 /**
  * @author: st
@@ -11,24 +14,31 @@ import com.google.common.collect.PeekingIterator;
 public class MountainArray {
 
   public static void main(String[] args) {
-    int[] ints = new int[] {5, 6, 7, 8, 9, 5, 4, 3, 3, 2};
+    int[] ints = new int[] {1, 5, 6, 7, 8, 9, 10, 5, 4, 3, 3, 2};
 
-    System.out.println(peakIndexInMountainArray(ints));
+    System.out.println(peakIndexInMountainArray(ints));//6
+    System.out.println(withMax(ints));//6
   }
 
-	public static int peakIndexInMountainArray(int[] arr) {
-		int n = arr.length;
-		int left = 1, right = n - 2, ans = 0;
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			if (arr[mid] > arr[mid + 1]) {
-				ans = mid;
-				right = mid - 1;
-			} else {
-				left = mid + 1;
-			}
-		}
-		return ans;
-	}
+  public static int peakIndexInMountainArray(int[] arr) {
+    int left = 0, right = arr.length - 1, ans = 0;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (arr[mid] > arr[mid + 1]) {
+        ans = mid;
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return ans;
+  }
 
+  public static int withMax(int[] array) {
+
+    int asInt = Arrays.stream(array).max().getAsInt();
+    int index = Ints.indexOf(array, asInt);
+    return index;
+  }
 }
+
