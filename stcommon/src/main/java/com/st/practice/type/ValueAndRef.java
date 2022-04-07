271ae9ca429014996c7be5451a4188244cd2e0a4
songtao <songtaoxy@foxmail.com>
2022-04-07 14:03:31 +0800

    面试题: 值类型相等判断

---
+++ stcommon/src/main/java/com/st/practice/type/ValueAndRef.java
@@ -0,0 +1,35 @@
+package com.st.practice.type;
+
+import com.st.utils.string.StringUtils;
+
+/**
+ * @author: st
+ * @date: 2022/4/7 13:38
+ * @version: 1.0
+ * @description:
+ */
+public class ValueAndRef {
+
+  public static void main(String[] args) {
+    int a = 1;
+    int b = 1;
+    Integer c = new Integer(1);
+    Integer d = new Integer(1);
+
+    System.out.println(a == b); // true
+    System.out.println(a == 1); // true
+    System.out.println(1 == 1); // ture
+    System.out.println(c == d); // fale
+    System.out.println(c == 1); // true
+    System.out.println(c == a); // true
+
+    System.out.println("a" == "a"); // true
+    String s1 = "a";
+    String s2 = "a";
+    String s3 = new String("a");
+    System.out.println(s1 == s2); // true
+    System.out.println(s1 == "a"); // true
+    System.out.println(s3 == s1); // fale
+    System.out.println(s3 == "a"); // fale
+  }
+}
