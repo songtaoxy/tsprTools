12e26b281d47e58d19e3d89dd3cdb005b6566b82
songtao <songtaoxy@foxmail.com>
2022-04-19 13:39:22 +0800

    继承

---
+++ stcommon/src/main/java/com/designpattern/exetend/Parent.java
@@ -0,0 +1,22 @@
+package com.designpattern.exetend;
+
+import com.st.utils.string.StringUtils;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author: st
+ * @date: 2022/4/19 13:35
+ * @version: 1.0
+ * @description:
+ */
+public class Parent {
+
+	String name;
+	String age;
+
+	public void test() {
+
+		System.out.println("");
+	}
+
+}
---
+++ stcommon/src/main/java/com/designpattern/exetend/Son.java
@@ -0,0 +1,21 @@
+package com.designpattern.exetend;
+
+import com.designpattern.visitor.Man;
+
+/**
+ * @author: st
+ * @date: 2022/4/19 13:36
+ * @version: 1.0
+ * @description:
+ */
+public class Son extends Parent{
+
+	public void btest2(){
+		test();
+		System.out.println(name);
+	}
+	public static void main(String[] args) {
+
+	}
+}
+
