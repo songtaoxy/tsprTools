6892e6114e40d69eb70c4ce40b7d99e57d14cf08
songtao <songtaoxy@foxmail.com>
2022-05-02 00:15:34 +0800

    中介者模式

--- stcommon/src/main/java/com/designpattern/mediator/smarthouse/Colleague.java
+++ stcommon/src/main/java/com/designpattern/mediator/smarthouse/Colleague.java
@@ -3,7 +3,13 @@ package com.designpattern.mediator.smarthouse;
 import org.codehaus.plexus.interpolation.util.ValueSourceUtils;
 import org.sonatype.aether.spi.log.NullLogger;
 
-//同事抽象类
+
+/**
+ * 同事类 <br>
+ *<br>
+ * 如何抽取? Alarm, TV, ... 都是相互协作的"同事" <br>
+ * 当然, 也可以根据实际进行抽取, 比如 "Device 设备" <br>
+ */
 public abstract class Colleague {
 	private Mediator mediator;
 	public String name;
@@ -13,9 +19,11 @@ public abstract class Colleague {
 		this.mediator = mediator;
 		this.name = name;
 
+		System.out.println("\n=========start===========");
 		System.out.println(this+"..........");
 		System.out.println(this.getClass().getMethod("GetMediator", null));
 		System.out.println(super.getClass().getName());
+		System.out.println("=========== end ===========");
 
 
 	}
---
+++ stcommon/src/main/java/com/st/utils/json/fastjson2/Demo.java
@@ -0,0 +1,12 @@
+package com.st.utils.json.fastjson2;
+
+/**
+ * @author: st
+ * @date: 2022/4/29 17:35
+ * @version: 1.0
+ * @description:
+ */
+public class Demo {
+
+
+}
