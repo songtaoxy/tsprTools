9f3a342c7bbc02b0801e5b515e6cb5dbece57af0
songtao <songtaoxy@foxmail.com>
2022-08-31 16:35:06 +0800

    case: final 修饰的方法

--- stcommon/src/main/java/com/st/practice/file/DemoFile2.java
+++ stcommon/src/main/java/com/st/practice/file/DemoFile2.java
@@ -3,12 +3,14 @@ package com.st.practice.file;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
+import org.apache.commons.codec.binary.Base64;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -24,6 +26,9 @@ public class DemoFile2 {
 		String pathy = "/Users/songtao/downloads/y.txt";
 		String pathz = "/Users/songtao/downloads/z.txt";
 		String pathf = "/Users/songtao/downloads/ff.txt";
+		String p = "/Users/songtao/downloads/2022-08-15_20-40-19.png";
+		String p2 = "/Users/songtao/downloads/2022-08-15_20-40-20.png";
+		String p3 = "/Users/songtao/downloads/2022-08-15_20-40-21.png";
 
 		byte[] bytes = Files.readAllBytes(Paths.get(path));
 
@@ -31,13 +36,22 @@ public class DemoFile2 {
 		map.put("k1", "k1-value");
 		map.put("c", bytes);
 
-		map.put("d", new String(bytes, StandardCharsets.UTF_8));
+		map.put("d", new String(bytes, StandardCharsets.ISO_8859_1));
 
 
+		byte[] bytes4 = Files.readAllBytes(Paths.get(p));
+		String s3 = new String(bytes4,StandardCharsets.ISO_8859_1);
+		byte[] bytes5 = s3.getBytes(StandardCharsets.ISO_8859_1);
+		new FileOutputStream(p2).write(bytes5);
+		new FileOutputStream(p3).write(bytes4);
 
+		String s4 = Arrays.toString(bytes4);
+		//new FileOutputStream(p2).write(s4.getBytes());
+		//s4.getBytes()
 
+		String s5 = Base64.encodeBase64String(bytes4);
 
-
+		byte[] bytes6 = Base64.decodeBase64(s5);
 
 
 		String s2 = new String(bytes);
