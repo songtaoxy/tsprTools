f77d3978a392bfc0c30dbb7597e4aece4a33c25c
songtao <songtaoxy@foxmail.com>
2022-10-04 02:24:47 +0800

    test: hutool

--- stcommon/src/main/java/com/st/practice/hutool/HutoolDemo.java
+++ stcommon/src/main/java/com/st/practice/hutool/HutoolDemo.java
@@ -1,6 +1,13 @@
 package com.st.practice.hutool;
 
+import cn.hutool.core.convert.Convert;
 import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import com.alibaba.fastjson2.JSON;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author: st
@@ -14,12 +21,24 @@ public class HutoolDemo {
 		strUtilT();
 	}
 
-
 	public static void strUtilT() {
 
 		String s = "{} 很想测试这个工具, {}, And do you have?";
 		String format = StrUtil.format(s, "I", "you");
 		System.out.println(format);
+
+		List<String> list = new ArrayList<String>();
+		list.add("1");
+		list.add("2");
+
+		JSONObject o = new JSONObject();
+		o.put("k",list);
+
+		List<String> types = o.getObject("k",List.class);
+		System.out.println(JSON.toJSONString(types));
+
+		List<String> types2 = o.getObject("k",new TypeReference<List<String>>(){});
+		System.out.println(JSON.toJSONString(types2));
 
 	}
 }
