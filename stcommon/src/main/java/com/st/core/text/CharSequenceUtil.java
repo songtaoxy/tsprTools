21a248329bcf4ffde9b70422d5d2f69414699e07
songtao <songtaoxy@foxmail.com>
2023-11-13 03:15:40 +0800

    工具类/hutool/整理到自己的工具类中

--- stcommon/src/main/java/com/st/core/text/CharSequenceUtil.java
+++ stcommon/src/main/java/com/st/core/text/CharSequenceUtil.java
@@ -424,4 +424,31 @@ public class CharSequenceUtil {
         return str1.toString().regionMatches(ignoreCase, start1, str2.toString(), start2, length);
     }
 
+
+
+
+    /**
+     * 格式化文本, {} 表示占位符<br>
+     * 此方法只是简单将占位符 {} 按照顺序替换为参数<br>
+     * 如果想输出 {} 使用 \\转义 { 即可，如果想输出 {} 之前的 \ 使用双转义符 \\\\ 即可<br>
+     * 例：<br>
+     * 通常使用：format("this is {} for {}", "a", "b") =》 this is a for b<br>
+     * 转义{}： format("this is \\{} for {}", "a", "b") =》 this is \{} for a<br>
+     * 转义\： format("this is \\\\{} for {}", "a", "b") =》 this is \a for b<br>
+     *
+     * @param template 文本模板，被替换的部分用 {} 表示，如果模板为null，返回"null"
+     * @param params   参数值
+     * @return 格式化后的文本，如果模板为null，返回"null"
+     */
+    public static String format(CharSequence template, Object... params) {
+        if (null == template) {
+            return NULL;
+        }
+        if (ArrayUtil.isEmpty(params) || isBlank(template)) {
+            return template.toString();
+        }
+        return StrFormatter.format(template.toString(), params);
+    }
+
+
 }
