da55172573ec9360eddccb8adb6f43ca8b509ad6
songtao <songtaoxy@foxmail.com>
2024-11-02 12:45:59 +0800

    temp/test

---
+++ stcommon/src/main/java/com/st/practice/tmp/F1.java
@@ -0,0 +1,19 @@
+package com.st.practice.tmp;
+
+public class F1 {
+
+    protected void f() {}
+
+
+}
+
+class S1 extends F1 { }
+
+
+class Test {
+    public static void main(String[] args) {
+
+        S1 s1 = new S1();
+
+    }
+}
