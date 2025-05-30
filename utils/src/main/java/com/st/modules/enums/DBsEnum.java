432d994c6aeb21d70e13cff3bab807a3c624c599
songtao <songtaoxy@foxmail.com>
2025-05-30 13:42:58 +0800

    枚举/数据库表/字典

---
+++ utils/src/main/java/com/st/modules/enums/DBsEnum.java
@@ -0,0 +1,47 @@
+package com.st.modules.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * tables dict
+ */
+@AllArgsConstructor
+@Getter
+@ToString
+@Slf4j
+public enum DBsEnum implements BaseEnum{
+    // 应付单
+    ap_payablebill("100000", "ap_payablebill", "供应商应付单表(主)", null, null),
+    ap_payableitem("100005", "ap_payableitem", "供应商应付单行(子)", null, null),
+   // 凭证
+    gl_voucher("100010", "gl_voucher", "凭证表", null, null),
+    gl_detail("100011", "gl_detail", "凭证分录", null, null),
+    gl_dtlfreevalue("100012", "gl_dtlfreevalue", "凭证分录自定义项", null, null);
+
+
+    private final String code;
+    private final String name;
+    private final String des;
+    private final String active;
+    private final String ext;
+
+
+    public static DBsEnum getByCode(String code) {
+        return EnumUtils.getByCode(DBsEnum.class, code).orElse(null);
+    }
+
+    public static DBsEnum getByName(String name) {
+        return EnumUtils.getByName(DBsEnum.class, name).orElse(null);
+    }
+
+    public static String getNameByCode(String code) {
+        return EnumUtils.getNameByCode(DBsEnum.class, code);
+    }
+
+    public static String getCodeByName(String name) {
+        return EnumUtils.getCodeByName(DBsEnum.class, name);
+    }
+}
