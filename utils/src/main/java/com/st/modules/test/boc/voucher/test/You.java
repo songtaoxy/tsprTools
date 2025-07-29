f62ad968b1364e9597e35816bd7e66cb81620933
songtao <songtaoxy@foxmail.com>
2025-07-29 18:24:04 +0800

    test

---
+++ utils/src/main/java/com/st/modules/test/boc/voucher/test/You.java
@@ -0,0 +1,12 @@
+package com.st.modules.test.boc.voucher.test;
+
+public class You {
+
+    public static void main(String[] args) {
+
+        String y = "1234/我们";
+        String[] split = y.split("/");
+        System.out.println(split[0]);
+        System.out.println(split[1]);
+    }
+}
